122c6b8814190ec25c513800fe204adf73e26b5a
mihna123 <mihailonvojinovic@gmail.com>
2025-06-06 11:22:49 +0200

    Change slug to post

--- src/app/[post]/route.js
+++ src/app/[post]/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
-  const { slug } = await params;
-  return NextResponse.json({ message: `Hello ${slug}!` });
+	const { post } = await params;
+	return NextResponse.json({ message: `Hello ${post}!` });
 }
