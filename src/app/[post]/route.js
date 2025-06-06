d68cb154b8fc9372f12730d6351012f41c1690b7
mihna123 <mihailonvojinovic@gmail.com>
2025-06-06 13:16:25 +0200

    Add an api route for psoting and fetching comments

--- src/app/[post]/route.js
+++ src/app/[post]/route.js
@@ -1,6 +1,55 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { neon } from "@neondatabase/serverless";
+
+const allowCORSHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, OPTIONS, POST",
+	"Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function OPTIONS() {
+	return new Response(null, {
+		status: 204,
+		headers: allowCORSHeaders,
+	});
+}
 
 export async function GET(request, { params }) {
 	const { post } = await params;
-	return NextResponse.json({ message: `Hello ${post}!` });
+	const sql = neon(process.env.DATABASE_URL);
+	const comments = await sql.query("SELECT * FROM comments WHERE post = $1", [
+		post,
+	]);
+	return new NextResponse(
+		`
+        <ul class="simple-comments">
+            ${comments.map((c) => `<li><p>${c.username}:</p><p>${c.comment}</p></li>`).join("")}
+        </ul>`,
+		{ headers: { "content-type": "text/html" }, status: 200 },
+	);
+}
+
+/**
+ * @param {NextRequest} request
+ * */
+export async function POST(request, { params }) {
+	const formData = await request.formData();
+
+	const { post } = await params;
+	const username = formData.get("username");
+	const comment = formData.get("comment");
+
+	const sql = neon(process.env.DATABASE_URL);
+
+	await sql.query(
+		`INSERT INTO comments 
+            (post, username, comment, createddate) 
+            VALUES ($1,$2,$3, current_timestamp)`,
+		[post, username, comment],
+	);
+
+	return new NextResponse("Success", {
+		status: 200,
+		headers: allowCORSHeaders,
+	});
 }
